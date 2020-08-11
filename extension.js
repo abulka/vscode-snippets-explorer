04a5a56f4af74524fd151d1abbf787882e13e06a
Andy Bulka <abulka@gmail.com>
2020-08-11 14:18:25 +1000

    added winston logging

--- extension.js
+++ extension.js
@@ -3,6 +3,7 @@
 const vscode = require('vscode');
 const { snippet_inserter } = require('./lib/snippet_inserter');
 const { TreeDataProvider: SnippetTreeDataProvider } = require('./lib/snippet_tree')
+const { appLog: appLog } = require("./lib/app_logger")
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,7 +15,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "snippetsexplorer" is now active!');
+	appLog('Congratulations, your extension "snippetsexplorer" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
